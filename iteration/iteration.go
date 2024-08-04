ce770c4cd2c3019d4f9c7bd63be75c78e63b83ea
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-04 11:22:26 +0330

    added ExampleRepeat as documentation function

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,22 @@
 package iteration
 
-func Repeat(character string,count int) string {
+import "fmt"
+
+func Repeat(character string, count int) string {
 	var repeated string
-	for i:=0; i < 5 ; i++{
-		repeated+=character
+	for i := 0; i < count; i++ {
+		repeated += character
 	}
 	return repeated
+}
+
+// ExampleRepeat demonstrates the use of the Repeat function.
+func ExampleRepeat() {
+	result1 := Repeat("a", 3)
+	fmt.Println(result1)
+	//Output: aaa
+
+	result2 := Repeat("a", 10)
+	fmt.Println(result2)
+	//Output: aaaaaaaaaa
 }
--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,19 +1,33 @@
 package iteration
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
-func TestRepeat(t *testing.T){
-	repeated := Repeat("a",5)
+func TestRepeat(t *testing.T) {
+	t.Run("repeating 'a' 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
 
-	wanted:= "aaaaa"
+		wanted := "aaaaa"
 
-	if repeated!=wanted{
-		t.Errorf("expected %q but got %q", wanted, repeated)
-	}
+		if repeated != wanted {
+			t.Errorf("expected %q but got %q", wanted, repeated)
+		}
+	})
+	t.Run("repeating 'a' 995 times", func(t *testing.T) {
+		repeated := Repeat("a", 995)
+
+		wanted := strings.Repeat("a", 995)
+
+		if repeated != wanted {
+			t.Errorf("expected %q but got %q", wanted, repeated)
+		}
+	})
 }
 
-func BenchmarkRepeat(b *testing.B){
-	for i:=0; i<b.N; i++{
-		Repeat("a")
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 10)
 	}
 }
