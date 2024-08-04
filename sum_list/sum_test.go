678e9e90dfd8a2630f1429612bb34006e62d3467
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-04 11:47:29 +0330

    sum_list slice failing test (red)

--- sum_list/sum_test.go
+++ sum_list/sum_test.go
@@ -3,12 +3,25 @@ package sum_list
 import "testing"
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := [5]int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("wanted %q but got %q, given %v", want, got,numbers)
+		}
+	})
+
+	t.Run("collection of any size",func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5,6}
 
 	got := Sum(numbers)
-	want := 15
+	want := 21
 
 	if got != want {
-		t.Errorf("wanted %q but got %q", want, got)
+		t.Errorf("wanted %q but got %q, given %v", want, got,numbers)
 	}
+	})
 }
