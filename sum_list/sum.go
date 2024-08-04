bcc9e8e363fbb57c8194f0a47d9b415aee8ad520
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-04 12:21:24 +0330

    sum_list SumAll failing test (red)

--- sum_list/sum.go
+++ sum_list/sum.go
@@ -1,9 +1,13 @@
 package sum_list
 
-func Sum(numbers []int)int{
-	sum:= 0 
-	for _, number := range numbers{
-		sum+= number
+func Sum(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+func SumAll(numbers ...[]int)[]int {
+	return nil
 }
--- sum_list/sum_test.go
+++ sum_list/sum_test.go
@@ -1,27 +1,39 @@
 package sum_list
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
 
 		if got != want {
-			t.Errorf("wanted %q but got %q, given %v", want, got,numbers)
+			t.Errorf("wanted %q but got %q, given %v", want, got, numbers)
 		}
 	})
 
-	t.Run("collection of any size",func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5,6}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6}
 
-	got := Sum(numbers)
-	want := 21
+		got := Sum(numbers)
+		want := 21
 
-	if got != want {
-		t.Errorf("wanted %q but got %q, given %v", want, got,numbers)
-	}
+		if got != want {
+			t.Errorf("wanted %q but got %q, given %v", want, got, numbers)
+		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{2, 3}, []int{1, 6, 5})
+	want := []int{5, 12}
+
+	if !reflect.DeepEqual(got,want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
