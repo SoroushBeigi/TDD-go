a85979225e4970e148ea192934ebcfd04014cfbf
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-04 09:30:02 +0330

    empty name feature test (red)

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -6,12 +6,26 @@ import (
 )
 
 func TestHello(t *testing.T) {
-	name := "Soroush"
+	t.Run("saying hello + names",func (t *testing.T)  {
+	const name = "Soroush"
 	want := fmt.Sprintf("Hello, %s",name)
 
-	got := Hello("Soroush")
+	got := Hello(name)
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+	})
+
+	t.Run("saying Hello, world when empty name is provided",func (t *testing.T)  {
+		const name = ""
+		want := "Hello, World"
+	
+		got := Hello(name)
+	
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+		})
+	
 }
