ca79a914b2bea06e03a65addd2fd3124af245fee
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-05 10:55:56 +0330

    geometry more readablity refactor

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -17,17 +17,20 @@ func TestArea(t *testing.T) {
 	areaTests := []struct {
 		shape Shape
 		want  float64
+		name  string
 	}{
-		{shape: Rectangle{Width: 10, Height: 20}, want: 200.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+		{shape: Rectangle{Width: 10, Height: 20}, want: 200.0, name: "Rectangle area"},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793, name: "Circle area"},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0, name: "Triangle area"},
 	}
 
 	for _, areaTest := range areaTests {
-		got := areaTest.shape.Area()
-		if got != areaTest.want {
-			t.Errorf("got %g want %g", got, areaTest.want)
-		}
+		t.Run(areaTest.name, func(t *testing.T) {
+			got := areaTest.shape.Area()
+			if got != areaTest.want {
+				t.Errorf("%#v got %g want %g", areaTest.shape, got, areaTest.want)
+			}
+		})
 
 	}
 
