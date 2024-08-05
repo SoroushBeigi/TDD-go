5eea3f63aaf75a7fd74a96484dcac375be18e8ff
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-05 10:42:38 +0330

    geometry refactor using interfaces

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -13,22 +13,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles area", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 20.0}
-		got := rectangle.Area()
-		want := 200.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-	t.Run("circles area", func(t *testing.T) {
-		circle := Circle{10.0}
-		got := circle.Area()
-		want := 314.1592653589793
-
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+
+	t.Run("rectangles area", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 20.0}
+		checkArea(t, rectangle, 200.0)
+	})
+	t.Run("circles area", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
 	})
 }
