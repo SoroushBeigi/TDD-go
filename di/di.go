6b9e894b95a478a0c2bd2f65b4a07fa6168239c8
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-06 10:11:38 +0330

    di passed test (green)

--- di/di.go
+++ di/di.go
@@ -5,6 +5,6 @@ import (
 	"fmt"
 )
 
-func Greet(writer *bytes.Buffer,name string) {
-	fmt.Printf("Hello, %s", name)
+func Greet(writer *bytes.Buffer, name string) {
+	fmt.Fprintf(writer, "Hello, %s", name)
 }
