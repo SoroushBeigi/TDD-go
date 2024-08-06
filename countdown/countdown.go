39dfe4ac0c7ae20581b7b9bc803a4e366f3f3581
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-06 11:45:59 +0330

    countdown refactor to test sleeping before each print

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -6,6 +6,9 @@ import (
 	"time"
 )
 
+const write = "write"
+const sleep = "sleep"
+
 type Sleeper interface {
 	Sleep()
 }
@@ -22,6 +25,19 @@ type SpySleeper struct {
 
 func (s *SpySleeper) Sleep() {
 	s.Calls++
+}
+
+type SpyCountdownOperations struct{
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep(){
+	s.Calls=append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte)(n int, err error){
+	s.Calls = append(s.Calls, write)
+	return
 }
 
 const finalWord = "Go!"
