0ec8db7ac1834c60d7c576f14e09c15c4dbf0d89
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-06 11:18:11 +0330

    countdown mock failing test (red)

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -3,14 +3,34 @@ package countdown
 import (
 	"fmt"
 	"io"
+	"time"
 )
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
 	}
 	fmt.Fprint(out, finalWord)
 }
