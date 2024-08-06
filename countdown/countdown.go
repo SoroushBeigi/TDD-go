7b53836ab2a29c7c2225963448230ffabe2bd621
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-06 11:12:05 +0330

    countdown passed test (green)

--- countdown/countdown.go
+++ countdown/countdown.go
@@ -3,13 +3,14 @@ package countdown
 import (
 	"fmt"
 	"io"
-	"os"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprint(out, "3")
-}
+const finalWord = "Go!"
+const countdownStart = 3
 
-func main() {
-	Countdown(os.Stdout)
+func Countdown(out io.Writer) {
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+	fmt.Fprint(out, finalWord)
 }
