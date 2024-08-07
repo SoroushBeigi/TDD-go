8a476a4917fa2c34cfbeb668d4851d6f3b145425
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-07 15:05:02 +0330

    reflection refactor

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,7 +3,7 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
+	val := getValue(x)
 
 	if val.Kind() == reflect.Pointer {
 		val = val.Elem()
@@ -19,4 +19,13 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(i interface{}) reflect.Value {
+	val := reflect.ValueOf(i)
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+	return val
 }
