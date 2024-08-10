b807e83e6e4c9c92e851be3fe019982331cc4bbb
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-10 08:26:44 +0330

    reflection func failing test (red)

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -80,8 +80,8 @@ func TestWalk(t *testing.T) {
 		{
 			"maps",
 			map[string]string{
-				"City":   "Tehran",
-				"Age": "21",
+				"City": "Tehran",
+				"Age":  "21",
 			},
 			[]string{"Tehran", "21"},
 		},
@@ -99,4 +99,68 @@ func TestWalk(t *testing.T) {
 		})
 	}
 
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Cow":   "Moo",
+			"Sheep": "Baa",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Moo")
+		assertContains(t, got, "Baa")
+	})
+
+	t.Run("channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{21, "Tehran"}
+			aChannel <- Profile{30, "Paris"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Tehran", "Paris"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with func", func(t *testing.T) {
+		aFunc := func() (Profile, Profile) {
+			return Profile{21, "Tehran"}, Profile{55, "Amsterdam"}
+		}
+		var got []string
+		want := []string{"Tehran", "Amsterdam"}
+
+		walk(aFunc, func(input string) {
+			got = append(got, input)
+		})
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+}
+
+func assertContains(t testing.TB, list []string, item string) {
+	t.Helper()
+	contains := false
+	for _, x := range list {
+		if x == item {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %v to contain %q", list, item)
+	}
 }
