a228deabad708d4a808801a35b611be14f705c4b
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-10 12:35:30 +0330

    store_server modification for new tests

--- store_server/store_server.go
+++ store_server/store_server.go
@@ -1,28 +1,23 @@
 package storeserver
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 )
 
 type Store interface {
-	Fetch() string
-	Cancel()
+	Fetch(ctx context.Context) (string, error)
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		context := r.Context()
-		data := make(chan string, 1)
-		go func() {
-			data <- store.Fetch()
-		}()
-
-		select {
-		case d := <-data:
-			fmt.Fprint(w, d)
-		case <-context.Done():
-			store.Cancel()
+		data, err := store.Fetch(r.Context())
+		if err != nil {
+			fmt.Println(err)
+			return
 		}
+		fmt.Fprint(w, data)
+
 	}
 }
