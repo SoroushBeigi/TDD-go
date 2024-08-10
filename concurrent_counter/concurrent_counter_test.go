ef4f7255c35983ebcaaea6cae34f84f1e9a74242
s.masoumbeigi <sbeygi712@gmail.com>
2024-08-10 10:31:45 +0330

    concurrent_counter increment refactor

--- concurrent_counter/concurrent_counter_test.go
+++ concurrent_counter/concurrent_counter_test.go
@@ -5,14 +5,20 @@ import "testing"
 func TestCounter(t *testing.T) {
 	t.Run("incrementing 3 times", func(t *testing.T) {
 		counter := Counter{}
-		
+
 		counter.Inc()
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
+		assertCounter(t, counter, 3)
 
 	})
+
+}
+
+func assertCounter(t *testing.T, counter Counter, want int) {
+	t.Helper()
+	if counter.Value() != want {
+		t.Errorf("got %d, want %d", counter.Value(), want)
+	}
 }
